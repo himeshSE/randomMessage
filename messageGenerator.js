ffc3fa2bb88b4dc75143f38b692c13f079c8b17e
himeshSE <himeshsofteng@gmail.com>
2023-01-20 10:47:07 +0530

    first commit

---
+++ messageGenerator.js
@@ -0,0 +1,8 @@
+const messages = ['Airbus A380 seating capacity - 853', 'Airbus A330 seating capacity- 406','Airbus A320 - 180', 'B777 seating capacity - 350']
+
+//Function to generate numbers between 0 and 3 (0 and 3 inclusive) 
+const randNum = () =>{
+    return Math.floor(Math.random() * (3 - 0 + 1) + 0);
+}
+
+console.log(messages[randNum()]);
